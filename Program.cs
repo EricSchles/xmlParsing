a51f43f9bc285144c529da0f9fea9061e2463585
Eric Schles <ericschles@gmail.com>
2014-08-05 15:27:24 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xmlParsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml("<user name=\"John Doe\">A user node</user>");
+            Console.WriteLine(xmlDoc.DocumentElement.Name);
+            Console.WriteLine(xmlDoc.DocumentElement.InnerText);
+            Console.ReadKey();
+        }
+    }
+}
